fbbc9706e648403851d39fd197156399356d3ecb
leonelherreradkno@gmail.com <leonelherreradkno@gmail.com>
2019-05-26 15:45:06 -0300

    Primera version del programa

---
+++ Administracion_Personal/Administracion_Personal/Empleados.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Administracion_Personal
+{
+    public class Empleados
+    {
+        public string nombre;
+        public string apellido;
+        public DateTime F_Nac;
+        public string Est_Civil;
+        public string Genero;
+        public DateTime F_Ingreso;
+        public float sueldo_Base;
+        public string cargo;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        public string Estado_Civil
+        {
+            get { return Est_Civil; }
+            set { Est_Civil = value; }
+        }
+        public string _Genero
+        {
+            get { return Genero; }
+            set { Genero = value; }
+        }
+        public float Sueldo_Base
+        {
+            get { return sueldo_Base; }
+            set { sueldo_Base = value; }
+        }
+
+        public string Cargo
+        {
+            get { return cargo; }
+            set { cargo = value; }
+        }
+        public DateTime Fecha_Nac
+        {
+            get { return F_Nac; }
+            set { F_Nac = value; }
+        }
+        public DateTime Fecha_Ingreso
+        {
+            get { return F_Ingreso; }
+            set { F_Ingreso = value; }
+        }
+
+
+        public void Mostrar()
+        {
+            Console.WriteLine("--Nombre: {0}", nombre);
+            Console.WriteLine("--Apellido: {0}", apellido);
+            Console.WriteLine("--Fecha de Nacimiento: {0}", F_Nac.ToShortDateString());
+            Console.WriteLine("--Edad: {0}", Edad());
+            Console.WriteLine("--Estado Civil: {0}", Est_Civil);
+            Console.WriteLine("--Genero: {0}", Genero);
+            Console.WriteLine("--Fecha de Ingreso: {0}", F_Ingreso.ToShortDateString());
+            Console.WriteLine("--Antiguedad: {0}", Antiguedad());
+            Jubilacion();
+            Console.WriteLine("--Sueldo Base: ${0}", sueldo_Base);
+            Console.WriteLine("--Cargo: {0}", cargo);
+            Console.WriteLine("--Salario: ${0}", salario());
+        }
+        public int Antiguedad()
+        {
+            DateTime fecha = new DateTime((DateTime.Today - F_Ingreso).Ticks);
+            return fecha.Year - 1;
+        }
+
+        public int Edad()
+        {
+            DateTime fecha = new DateTime((DateTime.Today - F_Nac).Ticks);
+            return fecha.Year - 1;
+        }
+        public void Jubilacion()
+        {
+            int edad = Edad();
+            int edad_jub=0;
+            //Devuelve la cantidad de años restantes para la jubilación del empleado
+            if (Genero.Contains("Hombre") == true) edad_jub = 65;
+            else edad_jub = 60;
+
+            if (edad_jub < edad) {
+                Console.WriteLine("--Jubilacion: Ya esta disponible para el tramite");
+            }
+            else Console.WriteLine("--Jubilacion: Le faltan {0} anios para el tramite", edad_jub-edad);
+        }
+        public float salario() {
+            float salario = 0;
+            float adicional=0;
+            Random aleatorio = new Random();
+            int cant_hijos;
+            int ant = Antiguedad();
+
+            if (Edad() > 24)
+            {
+                cant_hijos = aleatorio.Next(0, 4);
+            }
+            else cant_hijos = aleatorio.Next(0, 1);
+
+
+            Console.WriteLine("--Cantidad de hijos de {0} {1}: {2}", this.nombre, this.apellido, cant_hijos);
+
+            if (ant < 20)
+            {
+                adicional += sueldo_Base * ant * (float)0.02;
+            }
+            else{
+                adicional += sueldo_Base * (float)0.25;
+            }
+
+            if (cargo.Contains("Ingeniero") == true || cargo.Contains("Especialista") == true)
+            {
+                adicional *= (float)1.50;
+            }
+            if (Est_Civil.Contains("Casado") == true || Est_Civil.Contains("Casada") == true)
+            {
+
+                if (cant_hijos > 2)
+                {
+                    adicional += (float)5000;
+                }
+            }
+            salario = sueldo_Base + adicional;
+
+            return salario;
+        }
+    }
+    enum Trabajo { Auxiliar, Administrativo, Ingeniero, Especialista, Investigador };
+}
---
+++ Administracion_Personal/Administracion_Personal/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Administracion_Personal;
+
+namespace Admimistracion_Personal
+{
+    class Program
+    {
+        static void Main(string[] args){
+            //List<Empleados> Personas = new List<Empleados>();
+            Empleados dat_emp = new Empleados();
+
+            //Fecha actual
+            DateTime Hoy = DateTime.Now;
+            String F_Act = Convert.ToString(Hoy);
+            Console.WriteLine("Fecha de hoy: {0}", F_Act);
+
+            //------------------------------AGREGAR EMPLEADOS 
+            //Agregar_Emp(Personas);
+            dat_emp = Agregar_Emp();
+            dat_emp.Mostrar();
+            //Menu(Personas, F_Act);
+
+            Console.ReadKey();
+        }
+        enum Nombres_Hombres { Javier, Luis, Ramon, Carlos, Pablo, Santiago, Ricardo, Alejandro, Nahuel, Agustin };
+        enum Nombres_Mujeres { Maria, Carolina, Rocio, Celeste, Patricia, Lourdes, Mirtha, Micaela, Antonela, Carmen };
+        enum Apelllidos { Sosa, Luna, Diaz, Herrera, Martinez, Jerez, Nieva, Cardozo, Brito, Villa};
+        enum Genero{Masculino, Femenino};
+        enum Estado_Civil { Casado, Soltero, Casada, Soltera };
+
+
+        //------------------------------AGREGAR EMPLEADOS
+        static Empleados Agregar_Emp(){
+            Empleados dat_emp = new Empleados();
+            //int cant_Per = 20;
+            Random aleatorio = new Random();
+
+            //Genero
+            dat_emp._Genero = Genero_Per();
+
+            //Nombre segun el genero
+            if ((dat_emp._Genero).Contains("Masculino") == true)
+            {
+                int Opcion_Nombre = aleatorio.Next(0, 9);
+                Nombres_Hombres Nombre = (Nombres_Hombres)Opcion_Nombre;
+                dat_emp.Nombre = Convert.ToString(Nombre);
+                int Opcion_Civil = aleatorio.Next(0, 1);
+                Estado_Civil Estado = (Estado_Civil)Opcion_Civil;
+                dat_emp.Estado_Civil = Convert.ToString(Estado);
+            }
+            else
+            {
+                int Opcion_Nombre = aleatorio.Next(0, 9);
+                Nombres_Mujeres Nombre = (Nombres_Mujeres)Opcion_Nombre;
+                dat_emp.Nombre = Convert.ToString(Nombre);
+                int Opcion_Civil = aleatorio.Next(2, 3);
+                Estado_Civil Estado = (Estado_Civil)Opcion_Civil;
+                dat_emp.Estado_Civil = Convert.ToString(Estado);
+            }
+
+            //Apellido
+            int Opcion_Apellido = aleatorio.Next(0, 9);
+            Apelllidos Ape = (Apelllidos)Opcion_Apellido;
+            dat_emp.Apellido = Convert.ToString(Ape);
+
+            //Fecha de Nacimiento
+            DateTime fecha_nac = new DateTime(aleatorio.Next(1955, 2002), aleatorio.Next(1, 12), aleatorio.Next(1, 29));
+            dat_emp.Fecha_Nac = fecha_nac;
+
+            //Fecha de ingreso
+            DateTime fecha_ing = new DateTime(aleatorio.Next(1980, 2019), aleatorio.Next(1, 12), aleatorio.Next(1, 29));
+            while (fecha_nac > fecha_ing)
+            {
+                fecha_ing = new DateTime(aleatorio.Next(1980, 2019), aleatorio.Next(1, 12), aleatorio.Next(1, 29));
+            }
+            dat_emp.Fecha_Ingreso = fecha_ing;
+
+            //Sueldo Basico
+            dat_emp.Sueldo_Base = (float)aleatorio.Next(10000, 25000);
+
+            //Cargo
+            int opcion_cargo = aleatorio.Next(0, 4);
+            Trabajo Cargo = (Trabajo)opcion_cargo;
+            dat_emp.Cargo = Convert.ToString(Cargo);
+
+            return dat_emp;
+        }
+        //Veo el Genero
+        static string Genero_Per(){
+            Random aleatorio = new Random();
+            int Opcion= aleatorio.Next(0,1);
+            Genero Sexo= (Genero)Opcion;
+            string Genero = Convert.ToString(Sexo);  
+            return Genero;
+        }
+
+
+
+        //------------------------------BUSCAR EMPLEADOS
+        /*static void Buscar_Emp(List<Empleados> Personas) {
+            Console.WriteLine("--Escriba el nombre, apellido o nombre completo: ");
+            string nom_bus = Convert.ToString(Console.ReadLine());
+            String[] partes_nom = nom_bus.Split(' ');
+            //Me fijo que el empleado exista
+            while (!Personas.Exists(x => x.nombre == partes_nom[0] || !Personas.Exists(y => y.apellido == partes_nom[1])))
+            {
+                Console.WriteLine("--No exite el empleado\nIngrese otro nombre\n--Escriba el nombre completo: ");
+                nom_bus = Convert.ToString(Console.ReadLine());
+                partes_nom = nom_bus.Split(' ');
+            }
+            Console.WriteLine(Personas.Where(x => x.nombre == partes_nom[0]).FirstOrDefault()); //Busco el empleado
+        }*/
+
+
+        //------------------------------ELIMINAR EMPLEADOS
+        /*static void Eliminar_Emp(List<Empleados> Personas){
+            Console.WriteLine("--Escriba el nombre completo: ");
+            string nom_elim = Convert.ToString(Console.ReadLine());
+            String[] partes_nom = nom_elim.Split(' ');
+            //Me fijo que el empleado exista
+            while (!Personas.Exists(x => x.nombre == partes_nom[0]) || !Personas.Exists(y => y.apellido == partes_nom[1])){
+                Console.WriteLine("--No exite el empleado\nIngrese otro nombre\n--Escriba el nombre completo: ");
+                nom_elim = Convert.ToString(Console.ReadLine());
+                partes_nom = nom_elim.Split(' ');
+            }
+            Personas.Remove(Personas.Where(x => x.nombre == partes_nom[0]).FirstOrDefault()); //Elimino el empleado
+        }*/
+        
+    }
+}
